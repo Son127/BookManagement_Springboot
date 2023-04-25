75a92b619765c124fc9a0865ea89a4b90e3aa816
DESKTOP-NUOPUIF\ITPS <a0105167504@gmail.com>
2023-04-25 10:38:43 +0900

    JwtAuthentication필터생성 및 포인트생성.

--- src/main/java/com/toyproject/bookmanagement/config/SecurityConfig.java
+++ src/main/java/com/toyproject/bookmanagement/config/SecurityConfig.java
@@ -7,10 +7,21 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.toyproject.bookmanagement.security.JwtAuthenticationEntryPoint;
+import com.toyproject.bookmanagement.security.JwtAuthenticationFilter;
+import com.toyproject.bookmanagement.security.jwt.JwtTokenProvider;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	private final JwtTokenProvider jwtTokenProvider;
+	private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
@@ -19,16 +30,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		
 		http.csrf().disable();
 		http.httpBasic().disable();
 		http.formLogin().disable();
+		
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS); // session사용 안하겠다.
 		
 		http.authorizeRequests()
 			.antMatchers("/auth/**") 
 			.permitAll()
 			.anyRequest()
-			.permitAll();
-//			.authenticated();
+			.authenticated()
+			.and()
+			.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class) //필터에서 UsernamePasswordAuthenticationFilter 검사함
+			.exceptionHandling()
+			.authenticationEntryPoint(jwtAuthenticationEntryPoint);
 	}
 }
---
+++ src/main/java/com/toyproject/bookmanagement/security/JwtAuthenticationFilter.java
@@ -0,0 +1,42 @@
+package com.toyproject.bookmanagement.security;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+import com.toyproject.bookmanagement.security.jwt.JwtTokenProvider;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends GenericFilterBean{
+
+	private final JwtTokenProvider jwtTokenProvider;
+	
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+			
+		HttpServletRequest httpRequest = (HttpServletRequest) request; // 다운캐스팅
+		String accessToken = httpRequest.getHeader("Authorization"); //기존토큰 가지고옴
+		accessToken = jwtTokenProvider.getToken(accessToken);
+		boolean validationFlag = jwtTokenProvider.vaildateToken(accessToken); //유효성 검사
+		
+		if(validationFlag) { //토큰을 받아서 SCH에 전달.
+			Authentication authentication = jwtTokenProvider.getAuthentication(accessToken);
+			SecurityContextHolder.getContext().setAuthentication(authentication); 
+		}
+		
+		chain.doFilter(request, response);
+	
+	}
+
+}
